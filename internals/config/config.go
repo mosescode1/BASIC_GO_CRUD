b19d2beb07499d96844a193dc5b01336b89dcbf4
mosescode1 <emosescode@gmail.com>
2025-03-15 14:00:45 +0100

    add configuration management for application settings

---
+++ internals/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+
+var AppConfig *Config
+
+
+type ServerConfig struct {
+	AppName string `env:"APP_NAME"`
+	Port string  `env:"PORT"`
+}
+
+
+type DatabaseConfig struct {
+	DatabaseName string `env:"DB_NAME"`
+}
+
+type Config struct {
+	Server ServerConfig
+	Database DatabaseConfig
+
+}
+
+func LoadConfigFromfile(filePath string) (*Config, error) {
+	if AppConfig != nil {
+		return AppConfig, nil
+	}
+
+	AppConfig = &Config{}
+	if err := cleanenv.ReadConfig(filePath, AppConfig); err != nil {
+		return nil, err
+	}
+
+	return AppConfig, nil
+}
