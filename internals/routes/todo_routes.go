a52006b94f77d6563a2d791f12b18e478df4ec6c
mosescode1 <emosescode@gmail.com>
2025-03-15 15:54:52 +0100

    add Todo routes for CRUD operations in the API

--- internals/routes/todo_routes.go
+++ internals/routes/todo_routes.go
@@ -1,1 +1,21 @@
 package routes
+
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mosescode1/BASIC_GO_CRUD.git/internals/handlers"
+)
+
+
+func SetUpTodoRoutes(app *fiber.App) {
+
+	todo := app.Group("/todos")
+	todoHandler := handlers.NewTodoHandler(context.Background())
+	todo.Post("/", todoHandler.CreateTodo)
+	todo.Get("/", todoHandler.GetAllTodos)
+	todo.Get("/:id", todoHandler.GetTodo)
+	todo.Patch("/:id", todoHandler.UpdatedTodo)
+	todo.Delete("/:id", todoHandler.DeleteTodo)
+}
+
