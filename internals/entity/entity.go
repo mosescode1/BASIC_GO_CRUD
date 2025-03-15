e8204224a92036a635d25243034a34be721acec0
mosescode1 <emosescode@gmail.com>
2025-03-15 15:54:36 +0100

    add Todo entity with fields for title, description, and completion status

--- internals/entity/entity.go
+++ internals/entity/entity.go
@@ -1,1 +1,14 @@
 package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+
+type Todo struct {
+	gorm.Model
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	IsCompleted bool   `json:"is_completed" gorm:"default:false;not null"`
+
+}
